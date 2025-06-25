7f806f18eb8f319598f2a41d2268c01f72c00337
Adam <73994535+adamtuft@users.noreply.github.com>
2025-06-25 17:08:08 +0100

    Multiple fixes/improvements

--- lib/utils.bash
+++ lib/utils.bash
@@ -6,3 +6,10 @@ fail() {
 	echo -e "asdf-clang-format: $*"
 	exit 1
 }
+
+coalesce_install_version() {
+	if [ "$ASDF_INSTALL_VERSION" = "latest" ]; then
+		# shellcheck disable=SC2154
+		ASDF_INSTALL_VERSION="$("${plugin_dir}/bin/latest-stable")"
+	fi
+}
