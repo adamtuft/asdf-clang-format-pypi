59c8d3cf127a59f836d6cb14df6c2f9368cc84c2
Adam <73994535+adamtuft@users.noreply.github.com>
2025-06-26 19:28:56 +0100

    Add tests etc

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,14 +2,34 @@
 
 set -euo pipefail
 
+stacktrace() {
+	# source: https://gitlab.com/kyb/autorsync/-/blob/master/utils.bash#L84
+	printf "Stack trace:\n"
+	local i=1 line file func
+	while read -r line func file < <(caller $i); do
+		printf "[%d] $file:$line in $func\n" "$i"
+		((i++))
+	done
+}
+
 fail() {
-	echo -e "asdf-clang-format: $*"
+	printf "asdf-%s: $* \n" "${PLUGIN_NAME:-unknown}"
+	stacktrace
 	exit 1
 }
 
+detect_plugin_name() {
+	local CURRENT_SCRIPT="${BASH_SOURCE[0]}"
+	PLUGIN_ROOT="$(dirname "$(dirname "${CURRENT_SCRIPT}")")"
+	if [ "${PLUGIN_NAME+x}" != x ]; then
+		PLUGIN_NAME="${PLUGIN_ROOT##*/}"
+	fi
+}
+
 coalesce_install_version() {
+	local get_latest_version="${1:?missing argument: path to latest-stable script}"
 	if [ "$ASDF_INSTALL_VERSION" = "latest" ]; then
 		# shellcheck disable=SC2154
-		ASDF_INSTALL_VERSION="$("${plugin_dir}/bin/latest-stable")"
+		ASDF_INSTALL_VERSION="$("$get_latest_version")"
 	fi
 }
